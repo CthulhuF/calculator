5f1bc308e4555878195bfedc8c1e81cca7ec32f1
CthulhuF <48179191+CthulhuF@users.noreply.github.com>
2019-06-27 14:11:50 +0300

    Add files via upload
    Third calculator

--- main2.c
+++ main2.c
@@ -15,31 +15,31 @@
 
 
 
-void nmb_file(FILE *file, char op, FILE *result) {
+void nmb_file(FILE *input, char operation, FILE *output) {
     float a, b, r;
-    if (op == '!')
-        fscanf(file, "%f", &a);
+    if (operation == '!')
+        fscanf(input, "%f", &a);
     else
-        fscanf(file, "%f %f", &a, &b);
-    switch (op) {
+        fscanf(input, "%f %f", &a, &b);
+    switch (operation) {
         case '+':
-            fprintf(result, "%.0f %c %.0f = %.0f", a, op, b, a + b);
+            fprintf(output, "%.0f %c %.0f = %.0f", a, operation, b, a + b);
             break;
         case '-':
-            fprintf(result, "%.0f %c %.0f = %.0f", a, op, b, a - b);
+            fprintf(output, "%.0f %c %.0f = %.0f", a, operation, b, a - b);
             break;
         case '*':
-            fprintf(result, "%.0f %c %.0f = %.0f", a, op, b, a * b);
+            fprintf(output, "%.0f %c %.0f = %.0f", a, operation, b, a * b);
             break;
         case '/':
-            fprintf(result, "%.0f %c %.0f = %.0f", a, op, b, a / b);
+            fprintf(output, "%.0f %c %.0f = %.0f", a, operation, b, a / b);
             break;
         case '!':
             r = 1;
             for (int i = 1; i <= a; i++) {
                 r = r * i;
             }
-            fprintf(result, "%.0f ! = %.0f", a, r);
+            fprintf(output, "%.0f ! = %.0f", a, r);
             break;
             break;
         case '^':
@@ -48,103 +48,103 @@ void nmb_file(FILE *file, char op, FILE *result) {
                 for (int i = 1; i < b; i++) {
                     a = a * r;
                 }
-                fprintf(result, "%.0f ^ %.0f = %.0f", r, b, a);
+                fprintf(output, "%.0f ^ %.0f = %.0f", r, b, a);
             } else
-                fprintf(result, "%.0f ^ %.0f = 1", r, b);
+                fprintf(output, "%.0f ^ %.0f = 1", r, b);
             break;
             break;
 
     }
-}
+}//SCALAR OPERATIONS
 
 
-void vct_file(FILE *file, char op, FILE *result) {
+void vct_file(FILE *input, char operation, FILE *output) {
     float *V1, *V2;
     int size;
-    fscanf(file, "%i", &size);
+    fscanf(input, "%i", &size);
     V1 = calloc(size, sizeof(V1));
     V2 = calloc(size, sizeof(V2));
     for (int i = 0; i < size; i++)
-        fscanf(file, "%f", &V1[i]);
+        fscanf(input, "%f", &V1[i]);
     for (int i = 0; i < size; i++)
-        fscanf(file, "%f", &V2[i]);
+        fscanf(input, "%f", &V2[i]);
     float r = 0;
-    switch (op) {
+    switch (operation) {
         case '+':
-            fprintf(result, "(");
+            fprintf(output, "(");
             for (int i = 0; i < size; i++) {
                 if (i == size - 1)
-                    fprintf(result, "%.0f", V1[i]);
+                    fprintf(output, "%.0f", V1[i]);
                 else
-                    fprintf(result, "%.0f ", V1[i]);
+                    fprintf(output, "%.0f ", V1[i]);
             }
-            fprintf(result, ") + (");
+            fprintf(output, ") + (");
             for (int i = 0; i < size; i++) {
                 if (i == size - 1)
-                    fprintf(result, "%.0f", V2[i]);
+                    fprintf(output, "%.0f", V2[i]);
                 else
-                    fprintf(result, "%.0f ", V2[i]);
+                    fprintf(output, "%.0f ", V2[i]);
             }
-            fprintf(result, ") = (");
+            fprintf(output, ") = (");
             for (int i = 0; i < size; i++) {
                 if (i == size - 1)
-                    fprintf(result, "%.0f", V1[i] + V2[i]);
+                    fprintf(output, "%.0f", V1[i] + V2[i]);
                 else
-                    fprintf(result, "%.0f ", V1[i] + V2[i]);
+                    fprintf(output, "%.0f ", V1[i] + V2[i]);
             }
-            fprintf(result, ")");
+            fprintf(output, ")");
             break;
         case '-':
-            fprintf(result, "(");
+            fprintf(output, "(");
             for (int i = 0; i < size; i++) {
                 if (i == size - 1)
-                    fprintf(result, "%.0f", V1[i]);
+                    fprintf(output, "%.0f", V1[i]);
                 else
-                    fprintf(result, "%.0f ", V1[i]);
+                    fprintf(output, "%.0f ", V1[i]);
             }
-            fprintf(result, ") + (");
+            fprintf(output, ") + (");
             for (int i = 0; i < size; i++) {
                 if (i == size - 1)
-                    fprintf(result, "%.0f", V2[i]);
+                    fprintf(output, "%.0f", V2[i]);
                 else
-                    fprintf(result, "%.0f ", V2[i]);
+                    fprintf(output, "%.0f ", V2[i]);
             }
-            fprintf(result, ") = (");
+            fprintf(output, ") = (");
             for (int i = 0; i < size; i++) {
                 if (i == size - 1)
-                    fprintf(result, "%.0f", V1[i] - V2[i]);
+                    fprintf(output, "%.0f", V1[i] - V2[i]);
                 else
-                    fprintf(result, "%.0f ", V1[i] - V2[i]);
+                    fprintf(output, "%.0f ", V1[i] - V2[i]);
             }
-            fprintf(result, ")");
+            fprintf(output, ")");
             break;
         case '*':
             for (int i = 0; i < size; i++)
                 r = r + V1[i] * V2[i];
-            fprintf(result, "<a,b> = %.0f", r);
+            fprintf(output, "<a,b> = %.0f", r);
             break;
     }
-}
+}//VECTOR OPERATIONS
 
 
 
 int main(void) {
     setvbuf(stdout, NULL, _IONBF, 0);
     setvbuf(stderr, NULL, _IONBF, 0);
-    char op, c;
-    FILE *file, *result;
-    file = fopen("input.txt", "r");
-    result = fopen("output.txt", "w");
-    while (!feof(file)) {
-        fscanf(file, " %c %c", &op, &c);
+    char operation, c;
+    FILE *input, *output;
+    input = fopen("input.txt", "r");
+    output = fopen("output.txt", "w");
+    while (!feof(input)) {
+        fscanf(input, " %c %c", &operation, &c);
         if (c == 'v') {
-            vct_file(file, op, result);
+            vct_file(input, operation, output);
         } else {
-            nmb_file(file, op, result);
+            nmb_file(input, operation, output);
         }
-        fprintf(result, "\n");
+        fprintf(output, "\n");
     }
-    fclose(file);
-    fclose(result);
+    fclose(input);
+    fclose(output);
     return EXIT_SUCCESS;
-}
+}//main
